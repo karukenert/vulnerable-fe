332bde0db1b0d6504ebcb36c3644be0206701597
Kenert Karu <kenert.karu@twn.ee>
2023-04-23 15:29:42 +0300

    add route

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
     children: [
       {
         path: '',
@@ -13,6 +12,14 @@ const routes = [
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
+      },
+      {
+        path: 'report',
+        name: 'Report',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "home" */ '@/views/Pdf.vue'),
       },
     ],
   },
